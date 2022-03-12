73bf8b7323297d322abdd09147b712d26c766ceb
Lucy Zhang <lucy-zhang@users.noreply.github.com>
2022-03-12 15:56:45 -0500

    rewrite to take more advantage of linear transformations

--- src/script.ts
+++ src/script.ts
@@ -4,8 +4,52 @@ const CANVAS_SIZE = 2 * RADIUS;
 type CanvasCoord = { cx: number; cy: number };
 
 type C = { readonly re: number; readonly im: number };
-type C2 = [C, C]; // Element of C^2, or CP^1.
-type Mat = [C, C, C, C]; // Row-major 2x2 matrix.
+type C2 = readonly [C, C]; // Element of C^2, or CP^1.
+type Mat = readonly [C, C, C, C]; // Row-major 2x2 matrix.
+
+const zero = fromReal(0);
+const one = fromReal(1);
+const id: Mat = fromCol(zero, one);
+
+function fromCol(v: C, w: C): Mat {
+  return [conj(w), v, conj(v), w];
+}
+
+function rotation(angle: number): Mat {
+  return fromCol(zero, fromPolar(1, -angle / 2));
+}
+
+function translation(dist: number): Mat {
+  return fromCol(fromReal(Math.sinh(dist / 2)), fromReal(Math.cosh(dist / 2)));
+}
+
+function invDet1(m: Mat): Mat {
+  return [m[3], neg(m[1]), neg(m[2]), m[0]];
+}
+
+function refl(line: Mat): Mat {
+  return mulMat(line, invDet1(conjMat(line)));
+}
+
+function inHalfPlaneFn(plane: Mat): (p: C2) => boolean {
+  const invPlane = invDet1(plane);
+  return function (p: C2): boolean {
+    return dehomogenize(apply(invPlane, p)).im >= 0;
+  };
+}
+
+function conjMat(m: Mat): Mat {
+  return [conj(m[0]), conj(m[1]), conj(m[2]), conj(m[3])];
+}
+
+function mulMat(m1: Mat, m2: Mat): Mat {
+  return [
+    add(mul(m1[0], m2[0]), mul(m1[1], m2[2])),
+    add(mul(m1[0], m2[1]), mul(m1[1], m2[3])),
+    add(mul(m1[2], m2[0]), mul(m1[3], m2[2])),
+    add(mul(m1[2], m2[1]), mul(m1[3], m2[3])),
+  ];
+}
 
 function fromPolar(mod: number, arg: number): C {
   return { re: mod * Math.cos(arg), im: mod * Math.sin(arg) };
@@ -54,7 +98,7 @@ function div(z1: C, z2: C): C {
   return realMul(1 / modSq(z2), mul(z1, conj(z2)));
 }
 
-function applyMoebius(m: Mat, p: C2): C2 {
+function apply(m: Mat, p: C2): C2 {
   return [
     add(mul(m[0], p[0]), mul(m[1], p[1])),
     add(mul(m[2], p[0]), mul(m[3], p[1])),
@@ -70,7 +114,11 @@ function applyAntiMoebius(m: Mat, p: C2): C2 {
   ];
 }
 
-function toC(p: C2): C {
+function homogenize(z: C): C2 {
+  return [z, one];
+}
+
+function dehomogenize(p: C2): C {
   return div(p[0], p[1]);
 }
 
@@ -92,81 +140,45 @@ function draw(canvasCtx: CanvasRenderingContext2D, params: Params): void {
   const sin_c = Math.sin(c);
   const cos_c = Math.cos(c);
 
-  // Csq and Bsq are squared Euclidean distances.
-  const Csq =
-    (cos_c + cos_a * cos_b - sin_a * sin_b) /
-    (cos_c + cos_a * cos_b + sin_a * sin_b);
-  const Bsq =
-    (cos_b + cos_c * cos_a - sin_c * sin_a) /
-    (cos_b + cos_c * cos_a + sin_c * sin_a);
-  const C = Math.sqrt(Csq);
-  const B = Math.sqrt(Bsq);
-  // sideSq is the squared Euclidean distance between the b and c vertices
-  // obtained via the law of cosines.
-  const sideSq = Csq + Bsq - 2 * C * B * cos_a;
-  const radiusSq =
-    sideSq /
-    (2 *
-      (1 +
-        cos_a * cos_b * cos_c -
-        sin_a * sin_b * cos_c -
-        sin_a * cos_b * sin_c -
-        cos_a * sin_b * sin_c));
-  const center = add(
-    fromReal(Math.sqrt(Csq)),
-    fromPolar(Math.sqrt(radiusSq), Math.PI / 2 - b)
+  const coshC = (cos_c + cos_a * cos_b) / (sin_a * sin_b);
+  const coshB = (cos_b + cos_c * cos_a) / (sin_c * sin_a);
+
+  const halfPlaneC: Mat = id;
+  const halfPlaneB: Mat = rotation(Math.PI + a);
+  const halfPlaneA: Mat = mulMat(
+    translation(Math.acosh(coshC)),
+    rotation(Math.PI - b)
   );
 
-  const matC: Mat = [fromReal(1), fromReal(0), fromReal(0), fromReal(1)];
+  const matC: Mat = refl(halfPlaneC);
+  const matB: Mat = refl(halfPlaneB);
+  const matA: Mat = refl(halfPlaneA);
 
-  const matB: Mat = [
-    fromPolar(1, a),
-    fromReal(0),
-    fromReal(0),
-    fromPolar(1, -a),
-  ];
+  const insideC = inHalfPlaneFn(halfPlaneC);
+  const insideB = inHalfPlaneFn(halfPlaneB);
+  const insideA = inHalfPlaneFn(halfPlaneA);
 
-  const matA: Mat = [
-    center,
-    fromReal(radiusSq - modSq(center)),
-    fromReal(1),
-    neg(conj(center)),
-  ];
+  const q_prHalfPlane: Mat = mulMat(
+    translation(Math.atanh(cos_a * Math.sqrt(1 - Math.pow(coshB, -2)))),
+    rotation(Math.PI / 2)
+  );
 
-  function insideC(z: C): boolean {
-    return z.im >= 0;
-  }
-
-  function insideB(z: C): boolean {
-    return arg(z) <= a && arg(z) >= -Math.PI + a;
-  }
-
-  function insideA(z: C): boolean {
-    return modSq(sub(z, center)) >= radiusSq;
-  }
-
-  const q_prCenterX = (B + 1 / B) / (2 * cos_a);
-  const q_prRadiusSq = q_prCenterX * q_prCenterX - 1;
-  const q_prCenter = { re: q_prCenterX, im: 0 };
-
-  function q_prRegion(z: C): 0 | 1 {
-    return modSq(sub(z, q_prCenter)) < q_prRadiusSq ? 1 : 0;
-  }
+  const inside_q_prHalfPlane = inHalfPlaneFn(q_prHalfPlane);
 
   const imgData = new ImageData(CANVAS_SIZE, CANVAS_SIZE);
   const data = imgData.data;
 
   for (let cy = 0; cy < CANVAS_SIZE; cy++) {
     for (let cx = 0; cx < CANVAS_SIZE; cx++) {
-      let z = complex({ cx: cx, cy: cy });
+      const z = complex({ cx: cx, cy: cy });
       if (z.re ** 2 + z.im ** 2 > 1) {
         continue;
       }
 
-      let insA = insideA(z);
-      let insB = insideB(z);
-      let insC = insideC(z);
-      let p: C2 = [z, { re: 1, im: 0 }];
+      let p: C2 = homogenize(z);
+      let insA = insideA(p);
+      let insB = insideB(p);
+      let insC = insideC(p);
       let numRefls = 0;
 
       while (!insA || !insB || !insC) {
@@ -177,15 +189,14 @@ function draw(canvasCtx: CanvasRenderingContext2D, params: Params): void {
         } else {
           p = applyAntiMoebius(matC, p);
         }
-        z = toC(p);
-        insA = insideA(z);
-        insB = insideB(z);
-        insC = insideC(z);
+        insA = insideA(p);
+        insB = insideB(p);
+        insC = insideC(p);
         numRefls += 1;
       }
 
       const odd = numRefls % 2;
-      const region = q_prRegion(z);
+      const region = inside_q_prHalfPlane(p) ? 1 : 0;
       const val = region * 128 + odd * 32;
 
       data[(cy * CANVAS_SIZE + cx) * 4 + 0] = val;
